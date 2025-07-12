98a5a24ecdd4a808f9aa74f211b6517e863594f7
German Escobar <germanjosearielescobarescobar@gmail.com>
2025-07-11 20:58:21 -0600

    Commit

---
+++ 5 Activity.py
@@ -0,0 +1,7 @@
+class Alumno:
+    def __init__(self, nombre, carne, carrera, note):
+        self.nombre = nombre
+        self.carne = carne
+        self.carrera = carrera
+        self.note = note
+
