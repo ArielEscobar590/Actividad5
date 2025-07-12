19a05648cb39b8fcf21ba7d1e0d1fbf6b62fcfbf
German Escobar <germanjosearielescobarescobar@gmail.com>
2025-07-11 21:08:51 -0600

    Commit

--- 5 Activity.py
+++ 5 Activity.py
@@ -9,10 +9,35 @@ class Colegio:
     def __init__(self):
         self.alumnos = []
 
-    def Registro(self, alumno):
+    def Registro(self):
+        name = input("Nombre: ")
+        carne = input("Carne: ")
+        carrera = input("Carrera: ")
+        note = input("Nota Final: ")
         self.alumnos.append(alumno)
 
     def Mostrar(self):
         print("Lista de Alumnos: ")
         for alumno in self.alumnos:
             print(f"{a}) Nombre: {alumno.nombre}, Carne: {alumno.carne}, Carrera: {alumno.carrera}, Nota Final: {alumno.note}")
+
+    def BuscarAlumno(self):
+        print("Ingrese el carné del alumno")
+
+
+def main():
+    students = Colegio()
+    print("1) Registrar Alumno")
+    print("2) Mostrar Lista de Alumnos")
+    print("3) Buscar Alumno por su carné")
+    print("4) Promedio notas de todos los alumnos")
+    op = input("Seleccione una opción:")
+
+    if op == "1":
+        students.Registro()
+
+
+
+
+
+main()
